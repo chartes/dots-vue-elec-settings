2df683a797dbb1df4f465ec288d506a18af67a74
vicpsl <jvboby@gmail.com>
2025-05-07 11:26:15 +0200

    initial commit

---
+++ theater/HomePageContent.vue
@@ -0,0 +1,29 @@
+<template>
+  <article class="about">
+    <div class="about-content app-width-padding">
+      <h2>A custom description for theater</h2>
+      <p>
+        This is a custom description the the theater collection.
+      </p>
+    </div>
+  </article>
+
+</template>
+<script>
+export default {
+  name: 'HomePageContent',
+
+  setup () {
+  }
+}
+</script>
+<style scoped>
+#article h2 {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 33px;
+  text-transform: none;
+}
+</style>

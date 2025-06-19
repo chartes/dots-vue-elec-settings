62f158ac117d8da8f0aa7b0c90de32b08cb9036b
vicpsl <jvboby@gmail.com>
2025-06-19 18:40:21 +0200

    WIP: add custom parameters (list/card choice, buttons texts, custom css) and restructuring custom settings in conf.json files

--- theater/HomePageContent.vue
+++ theater/HomePageContent.vue
@@ -1,7 +1,23 @@
 <template>
   <article class="about">
     <div class="about-content app-width-padding">
-      <h2>A custom description for theater</h2>
+      <h1>A custom description for theater</h1>
+      <h2>In the default custom component, you can use either: </h2>
+      <p>
+        <b>a DTS API collection description if available:</b>
+      </p>
+      <p>
+        {{ dtsCollDescription ? dtsCollDescription : 'No DTS API collection description available' }}
+      </p>
+      <p>
+        <b>a custom collection description (homePageSettings.collectionDescription) if available:</b>
+      </p>
+      <p>
+        {{ customCollDescription ? customCollDescription : 'No homePageSettings.collectionDescription available' }}
+      </p>
+      <p>
+        <b>...or use a complete custom default description:</b>
+      </p>
       <p>
         This is a custom description for the theater collection.
       </p>
@@ -10,20 +26,61 @@
 
 </template>
 <script>
+import { ref, watch } from 'vue'
+
 export default {
   name: 'HomePageContent',
+  props: {
+    dtsCollectionDescription: {
+      type: String,
+      required: false
+    },
+    customCollectionDescription: {
+      type: String,
+      required: false
+    }
+  },
+  setup (props) {
+    const dtsCollDescription = ref(props.dtsCollectionDescription)
+    const customCollDescription = ref(props.customCollectionDescription)
 
-  setup () {
+    watch(props, async (newProps) => {
+      dtsCollDescription.value = newProps.dtsCollectionDescription
+      customCollDescription.value = newProps.customCollectionDescription
+    }, { deep: true, immediate: true })
+
+    return {
+      dtsCollDescription,
+      customCollDescription
+    }
   }
 }
 </script>
 <style scoped>
-#article h2 {
+.about {
+  font-family: "Barlow", sans-serif;
+}
+.about h1 {
   padding-top: 20px;
   padding-bottom: 20px;
+  text-align: left;
+  font-size: 25px;
+  font-weight: 500;
+  line-height: 33px;
+  text-transform: none;
+  color: var(--text-color);
+}
+.about h2 {
+  padding-top: 20px;
+  padding-bottom: 20px;
+  text-align: left;
   font-size: 20px;
   font-weight: 500;
   line-height: 33px;
   text-transform: none;
+}
+.about p {
+  text-align: left;
+  text-indent: 0;
 }
 </style>

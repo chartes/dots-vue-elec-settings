4fb749ae4b4b00863f9a2c33a4739730ce5997fd
vicpsl <jvboby@gmail.com>
2025-09-09 17:00:37 +0200

    feat: edit custom configuration for the CLEM collection

---
+++ CLEM/about1.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="about">
+    <div class="title app-width-padding">CLEM</div>
+    <div class="about-content app-width-padding">
+      <p>
+        Le projet Clem a pour ambition de constituer une base de données répertoriant les inscriptions latines poétiques de l’âge moderne entendu au sens le plus large du terme, depuis le premier humanisme jusqu’à nos jours.
+      </p>
+      <p>
+        Le corpus étant à peu près infini, la base de données est vouée à une croissance perpétuelle ; on espère pouvoir la rendre participative à terme, afin que chacun puisse y déposer et y enregistrer les inscriptions qu’il aura repérées.
+      </p>
+      <p>
+        Le lancement de ce projet est financé dans le cadre de l’équipex <a target="_blank" href="https://projet.biblissima.fr/fr">Biblissima+</a>.
+      </p>
+    </div>
+  </article>
+
+</template>
+<script>
+export default {
+  name: 'AboutFirstSection',
+
+  setup () {
+  }
+}
+</script>
+<style scoped>
+</style>
---
+++ CLEM/about2.vue
@@ -0,0 +1,33 @@
+<template>
+  <article class="about">
+    <div class="title app-width-padding">Signes critiques :</div>
+    <div class="about-content app-width-padding">
+      <p>
+        ( ) : développements d’abréviations
+      </p>
+      <p>
+        [ ] : restitutions de passages manquants
+      </p>
+      <p>
+        < > : correction par addition
+      </p>
+      <p>
+        Italique : correction par modification
+      </p>
+      <p>
+        Nous marquons la quantité des syllabes pour souligner les erreurs de versification.
+      </p>
+    </div>
+  </article>
+
+</template>
+<script>
+export default {
+  name: 'AboutSecondSection',
+
+  setup () {
+  }
+}
+</script>
+<style scoped>
+</style>

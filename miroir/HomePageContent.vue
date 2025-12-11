713af03f7e30e02e9cfc94953d09dbecd5a8fda2
vicpsl <jvboby@gmail.com>
2025-12-11 14:57:39 +0100

    fix: Home Pages (HomePageContent.vue) files use consistent h2

--- miroir/HomePageContent.vue
+++ miroir/HomePageContent.vue
@@ -56,14 +56,16 @@ export default {
   color: var(--text-color);
 }
 .home h2 {
+  margin: 0 !important;
   padding-top: 20px;
   padding-bottom: 20px;
   text-align: left;
   font-family: "Barlow", sans-serif;
   font-size: 20px;
   font-weight: 500;
-  line-height: 33px;
+  line-height: 28px;
   text-transform: none;
+  color: #4a4a4a !important;
 }
 .home p {
   text-align: left;

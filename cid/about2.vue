5bc28e8147c99ee6ac25413589bf4d8b77bef2b2
philippepons <philippe.pons@chartes.psl.eu>
2026-02-04 17:46:31 +0100

    ajoute d'un fichier de conf pour editsdepacification ; améliore les conf de la cid et de cjc

---
+++ cid/about2.vue
@@ -0,0 +1,165 @@
+<template>
+  <article class="about">
+    <!--<div class="title app-width-padding">CLEM</div>-->
+    <div class="about-content app-width-padding">
+      <h2>Liste des réunions publiées</h2>
+      <table>
+        <tbody><tr>
+          <td><b>Rencontre</b></td>
+          <td><b>Titre et année du volume</b></td>
+          <td><b>Eds. scientifiques</b></td>
+          <td><b>Ed. commercial</b></td>
+          <td><b>Pages</b></td>
+          <td><b>Notice BECh</b></td>
+        </tr>
+        <tr>
+          <td>VI<sup>e</sup> congrès Munich 1983</td>
+          <td><a href="/cid/document/cid1983/"><em>Landesherrliche Kanzleien im Spätmittelalter...</em> 1984</a></td>
+          <td>G. Silagi</td>
+          <td>Arbeo-Gesellschaft</td>
+          <td>IX-765 et pl.</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=47918" target="_blank">8C382</a></td>
+        </tr>
+        <tr>
+          <td>Colloque Stuttgart 1985</td>
+          <td><a href="/cid/document/cid1985"><em>Cancelleria e cultura...</em> 1990</a></td>
+          <td>G. Gualdo</td>
+          <td>Archivio s. vaticano</td>
+          <td>VIII-341</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=11245" target="_blank">8C347</a></td>
+        </tr>
+        <tr>
+          <td>VII<sup>e</sup> congrès València 1986</td>
+          <td><a href="/cid/document/cid1986/"><em>Notariado público y documento privado...</em> 1989</a></td>
+          <td>J. Trenchs</td>
+          <td>Valencia, Conselleria de cultura educació i ciència</td>
+          <td>1330</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=57798" target="_blank">8ID283 (CTLES)</a></td>
+        </tr>
+        <tr>
+          <td>Colloque Madrid 1990</td>
+          <td><a href="/cid/document/cid1990"><em>Falsos y falsificaciones de documentos diplomaticos...</em> 1991</a></td>
+          <td>A. Canellas Lopez</td>
+          <td>Real Sociedad economica aragonesa de amigos del pais</td>
+          <td>189</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=32268" target="_blank">8C384</a></td>
+        </tr>
+        <tr>
+          <td>II<sup>e</sup> colloque de travail Porto 1991</td>
+          <td><a href="/cid/document/cid1991/"><em>Diplomatique royale du Moyen Âge XIII<sup>e</sup></em>-<em>XIV<sup>e</sup> siècles</em> 1996</a></td>
+          <td>J. Marques</td>
+          <td>Faculté des Lettres Porto</td>
+          <td>288 + 14</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=24725" target="_blank">8C404</a></td>
+        </tr>
+        <tr>
+          <td>III<sup>e</sup> colloque de travail Olomouc 1992</td>
+          <td><a href="/cid/document/cid1992/"><em>Typologie der Königsurkunden</em> [VII<sup>e</sup>-milieu XIII<sup>e</sup> siècle] 1998</a></td>
+          <td>J. Bistřický</td>
+          <td>Univerzita Palackého Olmütz</td>
+          <td>289</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=81916" target="_blank">8C408</a></td>
+        </tr>
+        <tr>
+          <td>VIII<sup>e</sup> congrès Innsbruck 1993 </td>
+          <td><a href="/cid/document/cid1993"><em>Die Diplomatik der Bischofsurkunde vor 1250</em> 1995</a></td>
+          <td>C. Haidacher et W. Köfler</td>
+          <td>Tiroler Landesarchiv</td>
+          <td>564</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=24706" target="_blank">8C383</a></td>
+        </tr>
+        <tr>
+          <td>IV<sup>e</sup> colloque de travail Séville 1994</td>
+          <td><a href="/cid/document/cid1994/"><em>Estudios sobre el Notariado Europeo (siglos XIV-XV)</em> 1997</a></td>
+          <td>P. Ostos et L. Pardo</td>
+          <td>Université Séville</td>
+          <td>107</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>V<sup>e</sup> colloque de travail Heidelberg 1996</td>
+          <td><a href="/cid/document/cid1996/"><em>Papsturkunden und europäisches Urkundenwesen</em> 1999 <em>AfD Bhft</em> 7</a></td>
+          <td>P. Herde et H. Jakobs</td>
+          <td>AfD (Böhlau)</td>
+          <td>XI-433</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=62470" target="_blank">8C311(7)</a></td>
+        </tr>
+        <tr>
+          <td>IX<sup>e</sup> congrès Gand 1998</td>
+          <td><a href="/cid/document/cid1998/"><em>La diplomatique urbaine</em> 2000</a></td>
+          <td>W. Prevenier et Th. De Hemptinne</td>
+          <td>Garant</td>
+          <td>XIV-581</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=24727" target="_blank">8C410</a></td>
+        </tr>
+        <tr>
+          <td>VI<sup>e</sup> colloque de travail Princeton-New York 1999</td>
+          <td><a href="/cid/document/cid1999"><em>Charters, cartularies and archives</em> 2002</a></td>
+          <td>A. Kosto et A. Winroth</td>
+          <td>Pontifical Institute of mediaeval studies</td>
+          <td>X-185</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=14496" target="_blank">8O1919(17)</a></td>
+        </tr>
+        <tr>
+          <td>X<sup>e</sup> congrès Bologne 2001</td>
+          <td><a href="http://www.archivi.beniculturali.it/dga/uploads/documents/Saggi/Saggi_83.pdf" target="_blank"><em>La  diplomatica dei documenti giudiziari...</em> 2004</a></td>
+          <td>G. Nicolaj</td>
+          <td>Direzione generale per gli archivi</td>
+          <td>XV-555</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=24685" target="_blank">8C417(11)</a></td>
+        </tr>
+        <tr>
+          <td>XI<sup>e</sup> congrès Troyes 2003</td>
+          <td><a href="/cid/document/cid2003/"><em>La langue des actes</em> 2004</a></td>
+          <td>O. Guyotjeannin</td>
+          <td>École des chartes</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>VII<sup>e</sup> colloque de travail Bonn 2005</td>
+          <td><em>Diplomatik im 21. Jhdt = AfD</em> t. 52 (2006) p. 233-673 et t. 53 (2007) p. 367-417</td>
+          <td>W. Koch et T. Kölzer</td>
+          <td>AfD (Böhlau)</td>
+          <td>492</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=4550" target="_blank">8Q223</a></td>
+        </tr>
+        <tr>
+          <td>XII<sup>e</sup> congrès St Pölten 2009</td>
+          <td><a href="/cid/document/cid2009"><em>Regionale Urkundenbücher</em></a></td>
+          <td>T. Kölzer, W. Rosner, R. Zehetmayer</td>
+          <td>Niederöster. Landesarchiv</td>
+          <td>327</td>
+          <td>-</td>
+        </tr>
+        <tr>
+          <td>VIII<sup>e</sup> colloque de travail Amsterdam 2010</td>
+          <td><em>Charters in medieval society = AfD</em> t. 57 (2011), p. 205-425</td>
+          <td>W. Koch et T. Kölzer</td>
+          <td>AfD (Böhlau)</td>
+          <td>221</td>
+          <td><a href="https://catalogue.chartes.psl.eu/cgi-bin/koha/opac-detail.pl?biblionumber=4550" target="_blank">8Q223</a></td>
+        </tr>
+        <tr>
+          <td>XIII<sup>e</sup> congrès Paris 2012</td>
+          <td><a href="http://elec.enc.sorbonne.fr/cid2012/"><em>Les formulaires</em> 2016</a></td>
+          <td>O. Guyotjeannin, L. Morelle, S. P. Scalfati</td>
+          <td>École des chartes</td>
+          <td>-</td>
+          <td>-</td>
+        </tr>
+      </tbody></table>
+    </div>
+  </article>
+
+</template>
+<script>
+export default {
+  name: 'AboutFirstSection',
+
+  setup () {
+  }
+}
+</script>
+<style scoped>
+</style>

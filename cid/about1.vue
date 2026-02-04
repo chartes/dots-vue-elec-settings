d34427a9cbca34da2218daf1f3f2bcf307779fd0
philippepons <philippe.pons@chartes.psl.eu>
2026-02-04 17:46:31 +0100

    ajoute d'un fichier de conf pour editsdepacification ; améliore les conf de la cid et de cjc

---
+++ cid/about1.vue
@@ -0,0 +1,24 @@
+<template>
+  <article class="about">
+    <!--<div class="title app-width-padding">CLEM</div>-->
+    <div class="about-content app-width-padding">
+      <h2>Le projet</h2>
+      <p>Fondée de façon informelle par un petit groupe de savants européens en 1965, la Commission internationale de diplomatique (<a href="https://cidipl.org/" target="_blank">CID</a>) a été officiellement rattachée en 1970 au Comité international des sciences historiques (<a href="https://www.cish.org/" target="_blank">CISH</a>), sous le statut de « commission interne ». Depuis sa création, elle œuvre au dialogue entre écoles nationales, et à la mise au point d’outils de travail, tel le <i>Vocabulaire international de la diplomatique</i> (2e éd., València, 1997; <a href="https://www.cei.lmu.de/VID/" target="_blank">version en ligne</a>).</p>
+      <p>Outre le patronage de plusieurs initiatives éditoriales, la CID assure la tenue de nombreuses rencontres organisées par un ou plusieurs de ses membres, certaines dans le cadre des congrès internationaux du CISH. Des réunions internes tournent autour de problèmes de méthodologie ou de thèmes spécialisés ; en dehors de réunions plus informelles, ont été ainsi tenus de 1972 à 2010 seize rencontres, dites « colloques de travail » puis « colloques techniques ». Par ailleurs, des rencontres plus formalisées, ouvertes à d’autres chercheurs, brassant des sujets plus vastes autour d’un grand type d’actes, d'un thème, d’un angle d’approche de la documentation…, ont le label de « congrès internationaux de diplomatique » ; le dernier en date, treizième du nom, s’est déroulé en septembre 2012.</p>
+      <p>Au plan scientifique, la collection des actes des congrès et de certains colloques pose progressivement les jalons solides d’un vaste manuel européen de diplomatique. Matériellement, les actes sont édités sous la responsabilité des organisateurs de la rencontre, le plus souvent dans le pays-hôte, aux soins d’un éditeur commercial ou universitaire, parfois dans une revue comme <i>Archiv für Diplomatik</i>. Certains volumes sont de ce fait très mal représentés dans les bibliothèques, et même dans le cas contraire, l’éparpillement dissimule la puissante unité intellectuelle de l’entreprise.</p>
+      <p>Sous le patronage de la CID, avec l’accord des éditeurs des volumes imprimés originaux, l’École des chartes, joignant les efforts de sa bibliothèque, de sa direction de la recherche, et de son centre de recherches, a entrepris l'édition électronique progressive des volumes d’actes, consultables en mode texte. La numérisation des actes des congrès de 1983 et de 1998 a été effectuée avec le soutien financier du GDR Diplomatique (Groupe de recherche 3177 du Centre national de la recherche scientifique ).</p>
+      <p class="byline">Olivier Guyotjeannin</p>
+    </div>
+  </article>
+
+</template>
+<script>
+export default {
+  name: 'AboutFirstSection',
+
+  setup () {
+  }
+}
+</script>
+<style scoped>
+</style>

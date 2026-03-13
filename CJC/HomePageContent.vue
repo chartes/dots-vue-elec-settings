2a722ff3f4ea2f9509c9036de7bc42eb5df3d226
vicpsl <jvboby@gmail.com>
2026-03-13 11:04:28 +0100

    feat(HomePageContent): add dynamic application root url to be used for links within the app to all HomePageContent

--- CJC/HomePageContent.vue
+++ CJC/HomePageContent.vue
@@ -22,17 +22,21 @@ export default {
       type: String,
       required: false
     },
-    appRootUrl: {
+    applicationRootUrl: {
       type: String,
       required: true
     }
   },
   setup (props) {
-    const appRootUrl = ref(props.appRootUrl)
+    /* Use appRootUrl to create links within the application with the relevant base url for the current deployment */
+    const appRootUrl = ref(props.applicationRootUrl)
+    /* Use dtsCollDescription to display the DTS API 'description' data */
     const dtsCollDescription = ref(props.dtsCollectionDescription)
+    /* Use customCollDescription to display the homePageSettings.descriptionSection.collectionDescription data of the collection conf.json */
     const customCollDescription = ref(props.customCollectionDescription)
 
     watch(props, async (newProps) => {
+      appRootUrl.value = newProps.applicationRootUrl
       dtsCollDescription.value = newProps.dtsCollectionDescription
       customCollDescription.value = newProps.customCollectionDescription
     }, { deep: true, immediate: true })

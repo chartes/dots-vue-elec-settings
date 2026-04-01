63ce55fc7e65c6c2c05a8acc53f310d5235f96c0
philippepons <philippe.pons@chartes.psl.eu>
2026-04-01 10:34:13 +0200

    feat(dubourg.conf): add keepCollectionMetadata ; add about content

--- dubourg/about1.vue
+++ dubourg/about1.vue
@@ -1,33 +1,19 @@
+<script setup>
+</script>
 <template>
   <div class="about-content">
-
-    <p>
-      Achevé au plus tard au début de 1427, le formulaire d'Odart Morchesne,
-      ici édité dans la version la plus proche de l'auteur, est riche de quelque
-      270 modèles d'actes que les notaires et secrétaires du roi de France
-      pouvaient être amenés à composer.
-    </p>
-
-    <p>
-      Aucun recueil n'était jusque-là parvenu à ce degré de pertinence et de
-      variété dans le choix des textes, aucun n'avait encore organisé la matière
-      de façon aussi efficace et structurée.
-    </p>
-
-    <p>
-      De fait, la compilation de Morchesne forma le socle d'un grand nombre de
-      formulaires postérieurs, puis, au début du XVIe siècle, du
-      <i>Grand stille</i> officiel de la chancellerie.
-    </p>
-
-    <p>
-      Sa capacité de séduction ne tient pas qu'à son efficacité, mais aussi au
-      double souci de l'auteur de livrer des matériaux d'une actualité immédiate,
-      autant que des formules éprouvées ; il fait pénétrer de plain-pied dans
-      l'écritoire du notaire du roi en même temps que dans les années troubles du
-      « royaume de Bourges ».
-    </p>
-
+    <div>
+      <h2>La correspondance du chancelier Antoine Du Bourg (1535-1538)</h2>
+      <p class=" author byline">Olivier Poncet</p>
+      <p>Originaire d'Auvergne, Antoine Du Bourg (v. 1490-1538) a fait une carrière de magistrat au service de la monarchie en devenant successivement lieutenant civil au Châtelet de paris (1526), puis maître des requêtes (1532) puis président à mortier du parlement de Paris (1534), tout en recevant des commissions de confiance auprès de Louise de Savoie ou pour accompagner la répression contre les financiers après 1527. Au décès de son mentor et compatriote Antoine Duprat, il fut nommé le 16 juillet 1535 chancelier de France, office qu'il détint jusqu'à sa mort accidentelle le 28 octobre 1538. Il exerça cette fonction dans un contexte de guerre presque permanent (de février 1536 à juin 1538) ainsi qu'en témoigne son exceptionnelle correspondance passive.</p>
+      <p>Celle-ci est certainement le plus riche ensemble épistolaire administratif conservé pour ce niveau de responsabilité dans la France de la Renaissance, si l'on excepte les fonds du connétable de Montmorency conservés à Chantilly. Formé de plus de 1 200 lettres missives reçues par Du Bourg durant un peu plus de trois années, il touche à tous les domaines d'activité du chancelier à cette époque, de la justice aux finances royales en passant par la surveillance de l'imprimé et la politique économique du royaume. Il est inégalement réparti sur les quatre années de cancellariat de Du Bourg, les années 1536 et 1537 étant de loin les mieux documentées. Les lettres reçues par Du Bourg de tous les milieux administratifs, judiciaires, financiers ou militaires sont principalement conservées aux Archives nationales, sous les cotes J 965 et J 966, à la suite probablement d'une saisie consécutive à son décès, plutôt que dans le cadre du procès intenté à son successeur Guillaume Poyet. En dehors de quelques pièces isolées, un complément un peu substantiel concernant ses relations avec les diplomates français à Rome et en Suisse est enfin conservé au département des manuscrits occidentaux de la Bibliothèque nationale de France dans la collection Dupuy (ms. 303).</p>
+      <p>L'édition de la correspondance d'Antoine Du Bourg s'inscrit dans un projet plus large de publication électronique de correspondances de l'âge moderne (XV<sup>e</sup>-début XIX<sup>e</sup> siècle), qui a vocation à toucher dans un premier temps les domaines de l'histoire administrative, diplomatique, érudite et commerciale de la première modernité (XVI<sup>e</sup>-première moitié du XVII<sup>e</sup> siècle). Il s'agit de construire un modèle et une interface d'édition commune à toutes les correspondances non littéraires de l'époque moderne, facilitant ainsi la navigation et les recherches croisées sur des corpus qui peuvent aller de quelques dizaines à plusieurs centaines de lettres. Première composante de ce projet, l'édition de la correspondance d'Antoine du Bourg sert de pilote pour les futures correspondances éditées ; aussi pourra-t-elle présenter encore quelques imperfections.</p>
+      <p>Cette édition, placée sous la responsabilité scientifique d'Olivier Poncet, professeur à l'École nationale des chartes, est actuellement développée sur le plan informatique par Camille Desenclos, doctorante à l'École nationale des chartes.</p>
+    </div>
+    <div>
+      <h2>Caractéristiques de l'édition</h2>
+      <p>Au delà des caractéristiques habituelles de l'édition critique, une attention toute particulière a été accordée aux sommaires et à l'indexation. Pour chacune des lettres, un sommaire numéroté permet d'accéder directement au paragraphe du texte concerné. Chaque personne et chaque toponyme mentionnés a été identifié dans la mesure du possible et relié à un index; ces noms se présentent pour l'instant sous la forme de termes soulignés dans la présente édition. L'identification et les éventuels commentaires historiques peuvent être consultés soit via le lien inscrit sur le nom qui renvoie directement à l'entrée dans l'index, soit par un simple survol du nom qui affiche dans une info-bulle les informations issues de l'entrée d'index.</p>
+    </div>
   </div>
 </template>
 

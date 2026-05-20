ed4cf499c6afbd2e3f52acd32347f32dc61d47ce
vicpsl <jvboby@gmail.com>
2026-05-20 17:51:48 +0200

    feat(logos): add elec logos to comptes & encpos - proposal

---
+++ comptes/assets/images/ElecLogoSimple.vue
@@ -0,0 +1,157 @@
+<script setup>
+</script>
+<template>
+  <svg
+  viewBox="0 0 423.49875 339.58698"
+  class="dots-logo-png"
+  stroke-width="6"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  width="423.49875"
+  height="339.58698"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <defs id="defs2">
+    <clipPath id="clip-left">
+      <rect id="rect1" x="0" y="380" width="256" height="60"/>
+    </clipPath>
+    <clipPath id="clip-right">
+      <rect id="rect2" x="256" y="380" width="256" height="60"/>
+    </clipPath>
+  </defs>
+
+  <!-- Pages arrière et principales -->
+  <path
+    id="path2"
+    d="m 90,120 c 50,-20 120,-20 166,20 V 420 C 210,380 140,380 90,400 Z"
+    transform="translate(-77,-83.412659)"
+    clip-path="url(#clip-left)"
+  />
+  <path
+    id="path3"
+    d="m 85,120 c 55,-20 120,-20 171,20 V 420 C 205,380 140,380 85,400 Z"
+    transform="translate(-77,-91.412659)"
+    clip-path="url(#clip-left)"
+  />
+  <path
+    id="path4"
+    d="M 422,120 C 372,100 302,100 256,140 v 280 c 46,-40 116,-40 166,-20 z"
+    transform="translate(-77,-83.412659)"
+    clip-path="url(#clip-right)"
+  />
+  <path
+    id="path5"
+    d="M 427,120 C 372,100 307,100 256,140 v 280 c 51,-40 116,-40 171,-20 z"
+    transform="translate(-77,-91.412659)"
+    clip-path="url(#clip-right)"
+  />
+
+  <path
+    id="path6"
+    d="M 3,16.587341 C 15.455336,12.435561 27.91067,9.1456513 40.33023,6.8965113 87.7383,-1.6889287 134.62498,4.8909013 179,36.587341 V 316.58734 c -56,-40 -116,-40 -176,-20 z"
+  />
+
+  <path
+    id="path7"
+    d="m 355,16.587341 c -60,-19.9999997 -120,-19.9999997 -176,20"
+  />
+  <path id="path8" d="M 179,36.587341 V 316.58734" />
+  <path id="path9" d="m 355,296.58734 c -60,-20 -120,-20 -176,20" />
+
+  <!-- Lettre E courbée -->
+  <path
+    id="path10"
+    d="m 38,51.587341 v 59.999999 m 0,-59.999999 c 0,0 25,-7 45,-3 m -45,33 c 0,0 25,-7 45,-3 M 38,111.58734 c 0,0 25,-7 45,-3"
+    stroke="currentColor"
+    stroke-width="10"
+    fill="none"
+  />
+
+  <!-- Lignes d'écriture strictement équidistantes du bord de la page -->
+  <path
+    id="path11"
+    d="m 38,148.88354 c 13.21091,-5.59338 33.25419,-6.55554 52.86845,-5.65653 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705 M 37.77977,173.54712 c 13.21091,-5.59338 33.25419,-6.55554 52.86845,-5.65653 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705 M 37.77977,197.54712 c 13.21091,-5.59338 33.25419,-6.55554 52.86845,-5.65653 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705 M 37.77977,225.54712 c 13.21091,-5.59338 33.25419,-6.55554 52.86845,-5.65653 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705 M 37.77977,247.54712 c 13.21091,-5.59338 33.25419,-6.55554 52.86845,-5.65653 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705 M 99.86845,69.227011 c 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705 m -52.18724,10.10295 c 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705 M 98.86845,110.22701 c 19.37557,0.88808 34.9071,4.25538 51.18724,8.89705"
+    stroke="currentColor"
+    stroke-width="2"
+    style="stroke-width:4;stroke-dasharray:none"
+  />
+
+  <!-- Nuage de points et rectangles remplis -->
+  <rect id="rect15" x="278" y="56.587341" width="6" height="6" fill="currentColor"/>
+  <rect id="rect16" x="288" y="116.58734" width="6" height="6" fill="currentColor"/>
+  <rect id="rect17" x="308" y="166.58734" width="6" height="6" fill="currentColor"/>
+  <rect id="rect18" x="372.45822" y="100.59241" width="6" height="6" fill="currentColor"/>
+  <rect id="rect19" x="406.72153" y="108.34683" width="6" height="6" fill="currentColor"/>
+  <rect id="rect20" x="414.49875" y="141.64304" width="6" height="6" fill="currentColor"/>
+  <rect id="rect21" x="356.20758" y="225.50885" width="6" height="6" fill="currentColor"/>
+  <rect id="rect22" x="303.55948" y="142.63039" width="6" height="6" fill="currentColor"/>
+  <rect id="rect23" x="408.62277" y="197.39999" width="6" height="6" fill="currentColor"/>
+  <rect id="rect24" x="328" y="216.58734" width="6" height="6" fill="currentColor"/>
+  <rect id="rect25" x="348" y="76.587341" width="6" height="6" fill="currentColor"/>
+  <rect id="rect26" x="363" y="126.58734" width="6" height="6" fill="currentColor"/>
+  <rect id="rect27" x="383" y="206.58734" width="6" height="6" fill="currentColor"/>
+  <rect id="rect28" x="325.9595" y="31.493668" width="6" height="6" fill="currentColor"/>
+  <rect id="rect29" x="302.07343" y="251.9595" width="6" height="6" fill="currentColor"/>
+  <rect id="rect30" x="340.68353" y="259.64557" width="6" height="6" fill="currentColor"/>
+  <rect id="rect31" x="273" y="146.58734" width="8" height="14" fill="currentColor"/>
+  <rect id="rect32" x="293" y="76.587341" width="6" height="12" fill="currentColor"/>
+  <rect id="rect33" x="313" y="196.58734" width="10" height="16" fill="currentColor"/>
+  <rect id="rect34" x="333" y="96.587341" width="8" height="12" fill="currentColor"/>
+  <rect id="rect35" x="373.77469" y="257.48355" width="8" height="12" fill="currentColor"/>
+  <rect id="rect36" x="354.72406" y="142.4481" width="8" height="12" fill="currentColor"/>
+  <rect id="rect37" x="353" y="166.58734" width="8" height="14" fill="currentColor"/>
+  <rect id="rect38" x="383.11139" y="123.82785" width="8" height="14" fill="currentColor"/>
+  <rect id="rect39" x="399.36963" y="251.37469" width="8" height="14" fill="currentColor"/>
+  <rect id="rect40" x="295.09872" y="221.61771" width="8" height="14" fill="currentColor"/>
+  <rect id="rect41" x="373" y="236.58734" width="6" height="12" fill="currentColor"/>
+  <rect id="rect42" x="393" y="76.587341" width="10" height="16" fill="currentColor"/>
+  <rect id="rect43" x="323.18228" y="236.96204" width="8" height="14" fill="currentColor"/>
+  <rect id="rect44" x="349.27594" y="277.82532" width="8" height="14" fill="currentColor"/>
+  <rect id="rect45" x="303" y="46.587341" width="6" height="12" fill="currentColor"/>
+  <rect id="rect46" x="365.59241" y="21.124054" width="8" height="16" fill="currentColor"/>
+  <rect id="rect47" x="283" y="176.58734" width="12" height="22" fill="currentColor"/>
+  <rect id="rect48" x="303" y="96.587341" width="14" height="20" fill="currentColor"/>
+  <rect id="rect49" x="323" y="136.58734" width="12" height="24" fill="currentColor"/>
+  <rect id="rect50" x="343" y="196.58734" width="14" height="20" fill="currentColor"/>
+  <rect id="rect51" x="363" y="56.587341" width="12" height="22" fill="currentColor"/>
+  <rect id="rect52" x="383" y="156.58734" width="14" height="20" fill="currentColor"/>
+  <rect id="rect53" x="393" y="276.58734" width="12" height="22" fill="currentColor"/>
+  <rect id="rect54" x="403" y="36.587341" width="14" height="20" fill="currentColor"/>
+  <rect id="rect55" x="321.79999" y="58.387344" width="12" height="22" fill="currentColor"/>
+  <rect id="rect56" x="398" y="221.58734" width="14" height="20" fill="currentColor"/>
+  <rect id="rect57" x="389.51645" y="3" width="6" height="6" fill="currentColor"/>
+</svg>
+</template>
+
+<style scoped>
+
+.dots-logo-png {
+  width: 95%;
+  height: auto;
+  padding: 5%;
+
+  color: var(--dots-color, #000);
+
+
+  &:not(.has-banner) {
+    --dots-bg-color: color-mix(in srgb, currentColor, white 99%);
+
+    background-color: color-mix(in srgb, currentColor 95%, white 0%) !important;
+  }
+
+  &.has-banner {
+    --dots-bg-color: color-mix(in srgb, currentColor 50%, white);
+
+    /*background-color: color-mix(in srgb, currentColor 55%, white 0%) !important;*/
+  }
+}
+
+defs, path {
+  fill: transparent;
+  stroke: var(--dots-bg-color);
+}
+
+rect {
+  fill: var(--dots-bg-color);
+}
+</style>

0a5b3659825d07cae91294eccc244d72bedd7ff2
Maxime Popineau <mpopinea@enc.lan>
2026-09-07 11:45:31 +0200

    Sync Cartulaires, Chroniques, Christofle and Montferrand settings

--- christofle/HomePageContent.vue
+++ christofle/HomePageContent.vue
@@ -1,7 +1,26 @@
 <template>
-  <p>
-    Pierre Christofle fut notaire royal à Orléans de 1423 à 1450. Ses 387 notes,
-    rédigées entre le 1<sup>er</sup> janvier et le 29 décembre 1437, sont éditées
-    ici dans leur ensemble.
-  </p>
+  <div class="home-abstract">
+    <p>
+      Pierre Christofle fut notaire royal à Orléans de 1423 à 1450, attaché à la
+      prévôté pour qui il rédigeait des contrats portant le sceau de l’institution.
+      Comme ses confrères, il inscrivait dans un registre, sous une forme abrégée,
+      les conventions qu’il attestait. Ces notes, au nombre de 387, rédigées entre
+      le 1<sup>er</sup> janvier et le 29 décembre 1437 et conservées aux Archives
+      départementales du Loiret sous la cote 3E 10144, sont ici éditées dans leur
+      ensemble.
+    </p>
+    <p>
+      Indexée, l’édition permet l’enquête sur le lexique des actes. Elle accompagne
+      et complète une version imprimée, publiée sous le titre
+      <cite>Promettre, confesser, s’obliger devant Pierre Christofle, notaire royal
+      d’Orléans [1437]</cite>, dans la collection « Mémoires et documents de l’École
+      des chartes », qui propose un essai interprétatif de cette documentation.
+    </p>
+    <p>
+      La mise en ligne donne un accès rapide au contenu par plusieurs outils de
+      consultation, ainsi qu’une vue en parallèle des images numérisées du registre
+      et de leur transcription, afin de mieux comprendre la mise en scène de l’écrit
+      notarié.
+    </p>
+  </div>
 </template>
---
+++ comptes/HomePageContent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home-abstract">
+    <p>
+      Les Archives départementales du Puy-de-Dôme conservent une collection
+      remarquable de documents relatifs à la ville de Montferrand. Parmi eux, les
+      livres de comptes des consuls, rédigés en bas-auvergnat (langue d’oc), offrent
+      un témoignage exceptionnel sur la vie urbaine, l’administration et la langue
+      en Auvergne aux XIII<sup>e</sup> et XIV<sup>e</sup> siècles.
+    </p>
+    <p>
+      Faisant suite au plus ancien registre consulaire en langue d’oc (coté CC 154,
+      1259–1272), l’édition réunit ici les huit registres de comptes qui couvrent
+      les années 1273 à 1319. Elle donne accès au texte des registres, à l’index des
+      noms de personnes et de lieux ainsi qu’à un glossaire.
+    </p>
+  </div>
+</template>
